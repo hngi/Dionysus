f149aa4b0bb6d40fdff19ec5e0880af4d04b2d11
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-27 00:24:31 +0100

    added expense functionality

--- expense.php
+++ expense.php
@@ -1,40 +1,77 @@
 <?php include 'includes/header.php'?>
+<?php include 'includes/expense.php'?>
 <?php include 'includes/sidebar.php'?>
 
         <main role="main">
-            <section class="panel important">
-                <h2>Add a new item</h2>
+            <section class="panel important1">
+            <h2>Add a new item</h2>
+            <?php if(isset($success))  echo $success  ?>
+                    <div class="col-sm-12 mb-6">
+                        <form class="col text-center needs-validation" novalidate onsubmit="validate()" method="POST">
+                            <div class="col-md-12 mb-2">
+                              <select name="type1" class="form-control" required>
+                                  <option>Select Type</option>
+                                  <option value="card">Card</option>
+                                  <option value="cash">Cash</option>
+                                  <option value="others">Others</option>
+                              </select>
+                            </div>
+
+                              <div class="col-md-12 mb-2">
+                                  <input type="text" placeholder="What did you spend on?" name="item-name" class="form-control" title="item-name" required>
+                              </div>
+
+                              <div class="col-md-12 mb-2">
+                                  <input type="date" name="date" class="form-control" title="date" required>
+                              </div>
+
+                              <div class="col-md-12 mb-2">
+                                  <input type="text" placeholder="Amount - How much?" name="amount" class="form-control" title="amount" required>
+                              </div>
+
+                              <div class="col-md-12 mb-2">
+                              <textarea class="form-control" name="description" placeholder="Description"></textarea>
+                              </div>
+
+                              <div class="col-md-12 mb-2">
+                                <button class="btn btn-primary text-center" name="submit" type="submit" id="submit">Add New Expense</button>
+                            </div>
+                        </form>
+                    </div>
             </section>
-            <section class="panel important">
-            <div class="row">
-            <div id="form-box">
-<form>
-<div class="col-md-12 mb-6">
-     <label for="type1">Type:</label>
-      <select name="type1">
-          <option value="card">Card</option>
-          <option value="cash">Cash</option>
-          <option value="cryptocoin">Cryptocoin</option>
-          <option value="other">Other</option>
-      </select>
-     <div class="invalid-feedback">Please enter your email</div>
-    </div>
+            <section class="panel important1">
+            <div class="col-sm-12 mb-6">
+            <p></p>
+            <h2>Recent Expenses</h2>
+            <p></p>
+            <p></p>
+            <table>
+                
+                <?php 
+                if (!$recent_items) {
+                    echo 'You have no Recent Expenses';
+                } else {
+                   echo '<tr>';
+                echo '<th id="type" class="center">type</th>';
+                echo '<th>name</th>';
+                echo '<th id="date">date</th>';
+                echo '<th style="text-align: right" id="amount">amount</th>';
+                echo '</tr>';
+                foreach ($recent_items as $item) {
+                    echo '<tr>';
+                    echo '<td>'.$item['type'] .'</td>';
+                    echo '<td>'.$item['expense_Item'].'</td>';
+                    echo '<td>'.$item['expense_Date'].'</td>';
+                    echo '<td>'.'₦'.$item['expense_Cost'].'</td>';
+                    echo '</tr>';
+                } 
+                }
+                ?>
+                </table>
+                <a href="view_expense.php" class="btn btn-primary text-center"> View All Expenses</a>
+            </div>
+        </section>
+    </main>
 
-  <div>
-    <span>Name:</span> <input type="text" name="item-name" placeholder="What did you spend on?">
-  </div>
-
-  <div>
-    <span>Date:</span> <input type="date" name="date">
-  </div>
-
-  <div>
-    <span>Amount:</span> <input type="number" name="amount" placeholder="How much?">
-  </div>
-
-</form>
-</div>
-            </section>
-        </main>
-
-<?php include 'includes/footer.php'?>
+    <script src="js/signup.js"></script>
+    <?php include 'includes/footer.php'?>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,12 +2,8 @@
     <nav role="navigation">
             <ul class="main">
                 <li class="dashboard"><a href="dashboard.php">Dashboard</a></li>
-                <li class="write"><a href="expense.php">Expenses</a></li>
-                <li class="edit"><a href="#">Account</a></li>
-                <li class="comments"><a href="#">Voucher</a></li>
-                <li class="users"><a href="#">Purchases</a></li>
-                <li class="reports"><a href="#">Reports</a></li>
-                <li class="Transaction"><a href="#">Transaction</a></li>
+                <li class="write"><a href="expense.php">Add Expense</a></li>
+                <li class="edit"><a href="view_expense.php">View All Expenses</a></li>
             </ul>
         </nav>
         
