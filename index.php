e690ecd787abc32c739dad5b7ccf3d1cc77082e6
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2019-09-26 14:16:15 +0100

    index php file

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="styles/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<head>
+	<title>Financial Tracker</title>
+</head>
+<body>
+	<div class="container-fluid">
+            
+		<nav class="nav">
+			 <a class="navbar-brand" href="index.html">
+			   <img src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1569269285/logo_zrn1mx.png" width="30" height="30" class="d-inline-block align-top" alt="">
+			   Financial Tracker
+			 </a>
+			 
+					<ul class="nav justify-content-center">
+					  <li class="nav-item active">
+						<a class="nav-link" href="index.html">Home
+							  <span class="sr-only">(current)</span>
+							</a>
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link" href="#">Why Us</a>
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link" href="#">Pricing</a>
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link" href="#">Contact</a>
+					  </li>
+					</ul>
+				  
+		</nav>
+
+	<div class="row">
+			<div class="col-sm-5 mb-4">
+				<h1>Get your expenses done Together</h1>
+				<h5>Financial Management made easy</h5>
+				<div class="col text-center">
+					<a href="signup.php">
+					<button type="submit" class="btn  btn-outline-primary col-md-4 mb-4 btn-lg" id="signup" value="SignUp">Start here</button>
+				    </a>
+				</div>
+			</div>
+			<div class="col-sm-7 mb-4">
+
+			</div>   
+	</div>
+</div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</html>
