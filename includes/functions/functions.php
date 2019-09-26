9d32507d8a6999a783eec41a54915f7b1c29629d
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2019-09-26 14:17:17 +0100

    added functions for reset password pages

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -46,6 +46,65 @@
 
 
 
+     function getUserByEmail($dbconn, $email){
+                    $stmt = $dbconn->prepare("SELECT * FROM user WHERE email=:e");
+
+                    $stmt->bindParam(':e', $email);
+
+                    $stmt->execute();
+
+
+                    $row = $stmt->fetch(PDO::FETCH_BOTH);
+
+
+                    return $row;
+
+
+
+                }
+
+
+
+     function emailDoesNotExist($dbconn, $email) {
+        $result = false;
+
+        $stmt = $dbconn->prepare("SELECT email FROM user WHERE :e=email");
+
+        $stmt->bindParam(":e", $email);
+        $stmt->execute();
+
+        $count = $stmt->rowCount();
+
+        if($count == 0) {
+            $result = true;
+        }
+
+        return $result;
+    }
+
+
+
+   function updatePassword($dbconn, $input) {
+
+           
+            $hash = password_hash($input['password'], PASSWORD_BCRYPT);
+
+        $stmt = $dbconn->prepare("UPDATE user SET password=:pass WHERE user_ID=:uid");
+
+        $data = [
+           
+            ":uid" => $input['user_id'],
+             ":pass" => $hash
+        ];
+
+        $stmt->execute($data);
+    }
+
+
+
+
+
+
      function userLogin($dbconn, $input) {
 
         $result = [];
