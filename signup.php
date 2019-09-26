0b5501899dd65564ce62e9f76422bd85922d89c4
nnamdiobiesie <nnamdi_obiesie@yahoo.com>
2019-09-26 13:45:22 +0100

    fixed some bugs

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@ $errors = array();
 if(array_key_exists('submit', $_POST)){
     
     if(empty($_POST['name'])) {
-            $errors['name'] = "Please enter your full name";
+            $errors['name'] = "Please enter your user name";
         }
         
 
@@ -17,6 +17,7 @@ if(array_key_exists('submit', $_POST)){
        
         if(doesEmailExist($conn, $_POST['email'])) {
             $errors['email'] = "Email already exists";
+            
         }
 
         if(empty($_POST['password'])) {
@@ -37,11 +38,18 @@ if(array_key_exists('submit', $_POST)){
 
             doUserRegister($conn, $clean);
 
-            echo "Registration successful";
+            $success = "Registration successful | Kindly login to continue";
+            header("location:login.php?success=$success");
         }
     }
 
+
  ?>
+
+
+
+
+
 
 
 
@@ -64,104 +72,92 @@ if(array_key_exists('submit', $_POST)){
         <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
         <div class="container-fluid">
             
-                <nav class="navbar navbar-expand-lg bg-transparent static-top">
-                                <div class="container-fluid">
-                                  <a class="navbar-brand" href="#">
-                                        <img src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1569269285/logo_zrn1mx.png" alt=""><b style="color: rgba(0, 0, 0, 0.51);">FINANCIAL TRACKER</b>
-                                      </a>
-                                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="background-color: transparent; top: 15px; float: right; ">
-                                        <span class="navbar-toggler-icon"></span>
-                                      </button>
-                                  <div class="collapse navbar-collapse" id="navbarResponsive">
-                                    <ul class="navbar-nav ml-auto">
-                                      <li class="nav-item active">
-                                        <a class="nav-link" href="#" style="color: rgba(0, 0, 0, 0.51);">Home
-                                              <span class="sr-only">(current)</span>
-                                            </a>
-                                      </li>
-                                      <li class="nav-item">
-                                        <a class="nav-link" href="#" style="color: rgba(0, 0, 0, 0.51);">Why Us</a>
-                                      </li>
-                                      <li class="nav-item">
-                                        <a class="nav-link" href="#" style="color: rgba(0, 0, 0, 0.51);">Pricing</a>
-                                      </li>
-                                      <li class="nav-item">
-                                        <a class="nav-link" href="#" style="color: rgba(0, 0, 0, 0.51);">Contact</a>
-                                      </li>
-                                    </ul>
-                                  </div>
-                                </div>
-                              </nav>
+                <nav class="nav">
+                     <a class="navbar-brand" href="index.php">
+                       <img src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1569269285/logo_zrn1mx.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                       Financial Tracker
+                     </a>
+                     
+                            <ul class="nav justify-content-center md-3">
+                              <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home
+                                      <span class="sr-only">(current)</span>
+                                    </a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Why Us</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                              </li>
+                            </ul>
                           
                 </nav>
 
             <div class="row">
                     <div class="col-sm-5 mb-4">
-                        <form class="col text-center" action="" method="post" style="top: 5px;"> 
+                        <form class="col text-center needs-validation" novalidate onsubmit="validate()" method="POST"> 
                             <div class="container">
                                 <div class="row">
                                   <div class="col text-center">
-
-                                   <!--     <button type="button" class="btn btn-outline-primary col-sm-4 mb-4 btn-sm" id="login">Login</button>
-                                      <button type="button" class="btn  btn-outline-primary col-sm-4 mb-4 btn-sm" id="signup" value="SignUp"> SignUp</button> -->
+                                    <a href="login.php"><button type="button" class="btn btn-outline-primary col-sm-4 mb-4 btn-sm" id="login">Login</button></a>
+                                     <a href="#"><button type="button" class="btn  btn-outline-primary col-sm-4 mb-4 btn-sm" disabled id="signup">SignUp</button></a>
                                   </div>
                                 </div>
                               </div>
+                              <div class="col-md-12 mb-2">
+                                  <label for="validationCustom01"></label>
+                                  <input type="text" name="name" class="form-control" id="validationCustom01" placeholder="Username" value="" title="Enter Your Username" required>
+                                  <div class="invalid-feedback">Please enter a Username</div>
+                              </div>
                               <?php 
-                            $data = displayErrors($errors, 'name');
-                            echo $data;
-                              ?>
-                            <div class="err">
-                            <input class="col-sm-10" type="text" placeholder="Full Name" title="Your Full Name" name="name">
-                            </div>
-
-                            <?php 
                             $data = displayErrors($errors, 'email');
                             echo $data;
                               ?>
-                              <div class="err">
-                            <input class="col-sm-10" type="email" placeholder="Email Address" title="Enter Your Email Address" name="email">
+                              <div class="col-md-12 mb-2">
+                                  <label for="validationCustom01"></label>
+                                  <input type="email" class="form-control" id="validationCustom02" placeholder="Email Address" value="" title="Enter Your Email Address" name="email" required>
+                                  <div class="invalid-feedback">Please enter your email address</div>
+                              </div>
+                              <div class="col-md-12 mb-2">
+                                  <label for="validationCustom01"></label>
+                                  <input type="password" class="form-control" name="password" id="validationCustom01" placeholder="Password" value="" title="Password" required>
+                                  <div class="invalid-feedback">Please enter a password</div>
                               </div>
 
-                            <?php 
-                            $data = displayErrors($errors, 'password');
-                            echo $data;
-                              ?>
-                              <div class="err">
-                            <input class="col-sm-10" type="password" placeholder="Password" name="password">
-                              </div>
-
-                            <?php 
+                              <?php 
                             $data = displayErrors($errors, 'pword');
                             echo $data;
                               ?>
-
-                            <div class="err">
-                            <input class="col-sm-10" type="password" placeholder="Confirm Password" name="pword">
-                            </div>
-
-                            <input class="btn col-sm-10 btn-outline-primary" type="submit" placeholder="Confirm Password" name="submit">
-                            
-                          </form>
-
+                              <div class="col-md-12 mb-2">
+                                  <label for="validationCustom01"></label>
+                                  <input type="password" class="form-control" name="pword" id="validationCustom01" placeholder="Confirm Password" value="" title="Confirm Password" required>
+                                  <div class="invalid-feedback">Please confirm your password</div>
+                              </div>
+                              <div class="col-md-12 mb-2">
+                                <button class="btn btn-primary col-md-12 mb-4 text-center" name="submit" type="submit" id="submit">Sign up</button>
+                              </div>
+                        </form>
                     </div>
-                    
-                    
+                    <div class="col-sm-7 mb-4">
+
+                    </div>   
             </div>
         </div>
+        <script src="js/signup.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
-<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
 </html>
