93e0224533e3ff5d9bcb2760b19445fddaf0f079
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-28 00:48:21 +0100

    refactor dashboard made statistics functional

--- dashboard.php
+++ dashboard.php
@@ -1,30 +1,68 @@
-<?php include 'includes/header.php'?>
-<?php include 'includes/sidebar.php'?>
+<?php
+include 'includes/db/db_config.php';
+include 'includes/functions/functions.php';
+include 'includes/header.php';
+include 'includes/sidebar.php';
+$today_expenses_sum = todayExpenses($conn, $_SESSION['userid']);
+$weekly_expenses_sum = weeklyExpenses($conn, $_SESSION['userid']);
+$monthly_expenses_sum = monthlyExpenses($conn, $_SESSION['userid']);
+$yearly_expenses_sum = yearlyExpenses($conn, $_SESSION['userid']);
+?>
         <main role="main">
             <section class="panel important">
                 <h2><?php echo $_SESSION['username']; ?> Welcome to Your Dashboard </h2>
             </section>
             <section class="panel" id="panel">
-                <p><strong>Expenses this week</strong></p>
+                <p><strong>Today's Expenses</strong></p>
                 <br>
-                <h2>₦34,000</h2>
+                <h2>
+                <?php if ($today_expenses_sum == "") 
+                { 
+                    echo "₦0"; 
+                } 
+                else {
+                    echo '₦' . $today_expenses_sum;
+                }
+                ?>
+                </h2>
             </section>
             <section class="panel" id="panel">
-                <p><strong>Purchases this week</strong></p>
+                <p><strong>Expenses this week</strong></p>
                 <br>
-                <h2>₦34,000</h2>
+                <h2>
+                <?php if($weekly_expenses_sum==""){
+                    echo "₦0";
+                } else {
+                    echo '₦' . $weekly_expenses_sum;
+                }
+                ?>
+                </h2>
             </section>
             <section class="panel" id="panel">
                 <p><strong>Expenses this month</strong></p>
                 <br>
-                <h2>₦34,000</h2>
+                <h2>
+                <?php if ($monthly_expenses_sum==""){
+                    echo "₦0";
+                } else {
+                    echo '₦' . $monthly_expenses_sum;
+                }
+                ?>
+                </h2>
             </section>
             <section class="panel" id="panel">
                 <p><strong>Expenses this year</strong></p>
                 <br>
-                <h2>₦34,000</h2>
+                <h2>
+                <?php if($yearly_expenses_sum==""){
+                    echo "₦0";
+                } else {
+                    echo '₦' . $yearly_expenses_sum;
+                }
+                ?>
+                </h2>
             </section>
-            <section class="panel important1">
+            <section class="panel important">
                 <p><strong><i class="fas fa-signal"></i> Expenses this year</strong></p>
                 <hr>
                 <canvas id="myChart" width="400" height="300"></canvas>
@@ -50,32 +88,6 @@
                     });
                 </script>
 
-            </section>
-            <section class="panel important1">
-                <p><strong><i class="fas fa-chart-pie"></i> Yearly Expenses</strong></p>
-                <hr>
-                <canvas id="myChart1" width="400" height="300"></canvas>
-                <script>
-                    var ctx = document.getElementById('myChart1').getContext('2d');
-                    var myDoughnutChart = new Chart(ctx, {
-                        // The type of chart we want to create
-                        type: 'doughnut',
-
-                        // The data for our dataset
-                        data: {
-                            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                            datasets: [{
-                                label: 'Amount in ₦',
-                                backgroundColor: '#1aa093',
-                                borderColor: '#1aa093',
-                                data: [50000, 45000, 40000, 35000, 30000, 25000, 20000, 15000, 10000, 5000],
-                            }]
-                        },
-
-                        // Configuration options go here
-                        options: {}
-                    });
-                </script>
             </section>
 
 
