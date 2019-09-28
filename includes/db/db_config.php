da372e81632929eba3675744dbaf3f1116ef3bf1
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-28 10:25:33 +0100

    refactor db config

--- includes/db/db_config.php
+++ includes/db/db_config.php
@@ -20,7 +20,8 @@
     $username = $url["user"];
     $password = $url["pass"];
     $dbname = substr($url["path"], 1);
-    $conn = new mysqli($server, $username, $password, $dbname);
+    $conn = new PDO("mysql:host=".$server."; dbname=".$dbname, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 ?>
