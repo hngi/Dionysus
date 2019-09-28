312dacbede5bd309bdb5a815aee10afa5a79e3ea
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-28 10:15:35 +0100

    new heroku cleardb configuration

--- includes/db/db_config.php
+++ includes/db/db_config.php
@@ -1,18 +1,27 @@
 <?php
 
-    define('DBNAME', 'financialtracker');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
+    // define('DBNAME', 'financialtracker');
+    // define('DBUSER', 'root');
+    // define('DBPASS', '');
 
-    try{
+    // try{
 
-    $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+    // $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+    // } catch(PDOException $err) {
+
+    //     echo $err->getMessage();
+    // }
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+    $conn = new PDO($server, $username, $password, $dbname);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
-    } catch(PDOException $err) {
-
-        echo $err->getMessage();
-    }
 
 ?>
