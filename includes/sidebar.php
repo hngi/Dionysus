785321796be506b003794b279b05d5030300c6b4
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-29 06:32:21 +0100

    final changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,5 @@
 <div class="body">
-    <nav role="navigation">
+    <nav role="navigation" style="margin-top: 120px; height: 180px;">
             <ul class="main">
                 <li class="dashboard"><a href="dashboard.php">Dashboard</a></li>
                 <li class="write"><a href="expense.php">Add Expense</a></li>
