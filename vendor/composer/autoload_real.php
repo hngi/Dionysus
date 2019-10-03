98dff2387e86c770c5526e591c7698c283f96282
Daniel Ufeli <danielufeli@yahoo.com>
2019-10-03 21:43:48 +0100

    configure sendgrid

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit924a38e618e71708d8de0dc9c5d72e38
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit924a38e618e71708d8de0dc9c5d72e38::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire924a38e618e71708d8de0dc9c5d72e38($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire924a38e618e71708d8de0dc9c5d72e38($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
