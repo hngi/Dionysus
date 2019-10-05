e03e3029235d8db9966a34cc05d7c96e4eb36929
Seun Ogunyemi <54277718+seunogunyemi@users.noreply.github.com>
2019-10-05 10:36:22 +0100

    Changed pricing.html to pricing.php
    Also added FAQ.html to navbar

--- includes/header.php
+++ includes/header.php
@@ -59,7 +59,11 @@ if (isset($_GET['logout'])) {
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="pricing.html"><b>Pricing</b></a>
+                <a class="nav-link" href="pricing.php"><b>Pricing</b></a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="FAQ.html"><b>FAQ</b></a>
             </li>
 
             <li class="nav-item">
