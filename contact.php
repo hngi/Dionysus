71370051d6cc23239f1b5c914348a736314324cf
Kingsley <kingsleyumujeyan@gmail.com>
2019-10-05 16:20:35 +0100

    Contact Page updated

--- contact.php
+++ contact.php
@@ -127,7 +127,7 @@ if (array_key_exists('submit', $_POST)) {
                     <a class="nav-link" href="pricing.php"><b>Pricing</b></a>
                     </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="FAQ.html"><b>FAQ</b></a>
+                    <a class="nav-link" href="FAQ.html"><b>FAQs</b></a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#"><b>Contact Us</b></a>
